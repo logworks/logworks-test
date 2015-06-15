e6c334eb422392f34d16d9b3080423524c27f2ba
Arpith Siromoney <arpith@feedreader.co>
2015-06-15 17:14:45 +0530

    switch to uri for request, and single function for exports

--- new-api.js
+++ new-api.js
@@ -1,53 +1,56 @@
 var request = require('request');
 
-var API = function(url) {
+module.exports = function(url) {
   this.url = url;
-}
-API.logs = {};
-API.logs.create = function() {
-  return new Promise((resolve, reject) => {
-    request.post({url:this.url}, function(e, r, b) {
-      if (e) reject(e);
-      else resolve(JSON.parse(b));
-    });
-  });
-}
-API.logs.show = function(logid) {
-  return new Promise((resolve, reject) => {
-    request.get(this.url+"/"+logid, (e, r, b) => {
-      if (e) reject(e);
-      else resolve(JSON.parse(b));
-    });
-  });
-}
-API.entries = {};
-API.entries.create = function(logid, entry) {
-  var type = entry.type;
-  var data = entry.data;
-  return new Promise((resolve, reject) => {
-    request.post({url: this.url+"/"+logid, form: {"type":type, "data":data}}, (e, r, b) => {
-      if (e) reject(e);
-      else resolve(JSON.parse(b));
-    });
-  });
-}
-API.entries.edit = function(logid, entryid, entry) {
-  var type = entry.type;
-  var data = entry.data;
-  return new Promise((resolve, reject) => {
-    request.put({url: this.url+"/"+logid+"/"+entryid, form: {"type":type, "data":data}}, (e, r, b) => {
-      if (e) reject(e);
-      else resolve(JSON.parse(b));
-    });
-  });
-}
-API.entries.del = function(logid, entryid) {
-  return new Promise((resolve, reject) => {
-    request.del(this.url+"/"+logid+"/"+entryid, (e, r, b) => {
-      if (e) reject(e);
-      else resolve(JSON.parse(b));
-    });
-  });
-}
+  this.logs = {};
+  this.entries = {};
 
-module.exports = API;
+  this.logs.create = function() {
+    return new Promise((resolve, reject) => {
+      request.post({uri:this.url}, function(e, r, b) {
+        console.log(b);
+        console.log(e);
+        console.log(r);
+        if (e) reject(e);
+        else resolve(JSON.parse(b));
+      });
+    });
+  }
+  this.logs.show = function(logid) {
+    return new Promise((resolve, reject) => {
+      request.get(this.url+"/"+logid, (e, r, b) => {
+        if (e) reject(e);
+        else resolve(JSON.parse(b));
+      });
+    });
+  }
+
+  this.entries.create = function(logid, entry) {
+    var type = entry.type;
+    var data = entry.data;
+    return new Promise((resolve, reject) => {
+      request.post({uri: this.url+"/"+logid, form: {"type":type, "data":data}}, (e, r, b) => {
+        if (e) reject(e);
+        else resolve(JSON.parse(b));
+      });
+    });
+  }
+  this.entries.edit = function(logid, entryid, entry) {
+    var type = entry.type;
+    var data = entry.data;
+    return new Promise((resolve, reject) => {
+      request.put({uri: this.url+"/"+logid+"/"+entryid, form: {"type":type, "data":data}}, (e, r, b) => {
+        if (e) reject(e);
+        else resolve(JSON.parse(b));
+      });
+    });
+  }
+  this.entries.del = function(logid, entryid) {
+    return new Promise((resolve, reject) => {
+      request.del(this.url+"/"+logid+"/"+entryid, (e, r, b) => {
+        if (e) reject(e);
+        else resolve(JSON.parse(b));
+      });
+    });
+  }
+}
