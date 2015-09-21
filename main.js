d99be515953b08eaddb8909dc3f63cf3f208496d
Arpith Siromoney <arpith@feedreader.co>
2015-09-21 10:46:27 +0530

    add test to generate token & use it in the other tests

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let fetch = require('node-fetch');
+let base64url = require('base64-url');
 let Promise = require('bluebird');
 let API = require('logworks');
 let baseApiUrl = process.env.API_URL || "https://2p0tufq4kg.execute-api.us-east-1.amazonaws.com";
@@ -7,10 +8,8 @@ let LogWorks = new API(apiUrl, fetch, Promise);
 let maxLogSize = process.env.MAX_LOG_SIZE || 10;
 
 function randomString() {
-  let crypto = require('crypto');
-  let shasum = crypto.createHash('sha1');
-  shasum.update(Math.random().toString());
-  return shasum.digest('hex');
+  let randomb64string = crypto.randomBytes(6).toString('base64');
+  return base64url.escape(randomb64string);
 }
 
 function randomLogSize() {
@@ -21,9 +20,19 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function testCreateLog() {
+function testGetToken(label) {
+  return LogWorks.generateToken(label).then((token) => {
+    if (!token.id || !token.secret) {
+      console.log("Token wasn't generated");
+      Promise.reject();
+    }
+    return token;
+  });
+}
+
+function testCreateLog(label, token) {
   let data = {title: randomString(), description: randomString()};
-  return LogWorks.logs.create(data).then((log) => {
+  return LogWorks.logs.create(label, token, data).then((log) => {
     let savedData = log.get('data').toJS();
     if ((data.title !== savedData.title) || (savedData.description !== data.description)) {
       console.log("Title or description wasn't saved");
@@ -33,11 +42,11 @@ function testCreateLog() {
   });
 }
 
-function testEditLog(log) {
+function testEditLog(label, token, log) {
   var data = log.get('data').toJS();
   data.title = randomString();
   data.description = randomString();
-  return LogWorks.logs.edit(log.get('id'),data).then(() => {
+  return LogWorks.logs.edit(label, token, log.get('id'),data).then(() => {
     return LogWorks.logs.show(log.get('url'));
   }).then((updatedLog) => {
     let updatedData = updatedLog.get('data').toJS();
@@ -49,8 +58,8 @@ function testEditLog(log) {
   });
 }
 
-function testGenerateSignedURL(log) {
-  return LogWorks.logs.generateSignedURL(log.get('id')).then((response) => {
+function testGenerateSignedURL(label, token, log) {
+  return LogWorks.logs.generateSignedURL(label, token, log.get('id')).then((response) => {
     let resObj = response.toJS();
     if ((resObj.success !== 'true') || (!resObj.url)) {
       console.log("Couldn't generate signed url");
@@ -60,16 +69,16 @@ function testGenerateSignedURL(log) {
   });
 }
 
-function testAddEntries(log, count) {
+function testAddEntries(label, token, log, count) {
   var entrydata = [];
   for (let i=0; i<count; i++) {
     entrydata.push(randomString());
   }
-  let promiseArr = entrydata.map(d => LogWorks.entries.create(log.get('id'),{type:"text", data:d}));
+  let promiseArr = entrydata.map(d => LogWorks.entries.create(label, token, log.get('id'),{type:"text", data:d}));
   return Promise.all(promiseArr).then(() => {
     return LogWorks.logs.show(log.get('url'));
   }).then((log) => {
-    let promiseArr = log.toJS().entries.map(entryid => LogWorks.entries.show(log.get('id'), entryid));
+    let promiseArr = log.toJS().entries.map(key => LogWorks.entries.show(log.get('id'), key));
     return Promise.all(promiseArr).then((response) => {
       response.forEach((entry) => {
         if (entrydata.indexOf(entry.get('data')) === -1) {
@@ -82,13 +91,15 @@ function testAddEntries(log, count) {
   });
 }
 
-function testEditEntries(log) {
+function testEditEntries(label, token, log) {
   let logid = log.get('id');
-  let promiseArr = log.toJS().entries.map(entryid => LogWorks.entries.edit(logid, entryid, {type: 'text', data: 'foo'}));
+  let entry = {type: 'text', data: 'foo'};
+  let ids = log.toJS().entries.map(key => key.split('?')[0]);
+  let promiseArr = ids.map(entryid => LogWorks.entries.edit(label, token, logid, entryid, entry));
   return Promise.all(promiseArr).then(() => {
     return LogWorks.logs.show(log.get('url'));
   }).then((log) => {
-    let promiseArr = log.toJS().entries.map(entryid => LogWorks.entries.show(log.get('id'), entryid));
+    let promiseArr = log.toJS().entries.map(key => LogWorks.entries.show(log.get('id'), key));
     return Promise.all(promiseArr).then((response) => {
       response.forEach((entry) => {
         if (entry.get('data') != "foo") {
@@ -101,8 +112,9 @@ function testEditEntries(log) {
   })
 }
 
-function testDeleteEntries(log) {
-  let promiseArr = log.toJS().entries.map(entryid => LogWorks.entries.del(log.get('id'), entryid));
+function testDeleteEntries(label, token, log) {
+  let ids = log.toJS().entries.map(key => key.split('?')[0]);
+  let promiseArr = ids.map(entryid => LogWorks.entries.del(label, token, log.get('id'), entryid));
   return Promise.all(promiseArr).then(() => {
     return LogWorks.logs.show(log.get('url'));
   }).then((log) => {
@@ -114,9 +126,9 @@ function testDeleteEntries(log) {
   });
 }
 
-function testDeleteLog(log) {
+function testDeleteLog(label, token, log) {
   let logid = log.get('id');
-  return LogWorks.logs.del(logid).then(() => {
+  return LogWorks.logs.del(label, token, logid).then(() => {
     return LogWorks.logs.show(log.get('url'));
   }).then((log) => {
     if (log.get('id') === logid) {
@@ -131,16 +143,23 @@ function testDeleteLog(log) {
 
 function startTest() {
   var logid = '';
+  var token = '';
   let logsize = randomLogSize();
-  testCreateLog().then((log) => {
+  let label = randomString();
+  testGenerateToken(label).then((t) => {
+    token = t;
+    return testCreateLog(label, token);
+  }).then((log) => {
     logid = log.get('id');
-    return log;
-  }).then(testEditLog).then(testGenerateSignedURL).then((log) => {
-    return testAddEntries(log, logsize);
-  }).then(testEditEntries).then(testDeleteEntries).then(testDeleteLog).then(() => {
-    console.log("WORKS for log size: "+logsize+" (logid: "+logid+")");
-  }).catch((err) => {
-    console.log("FAILED for log size: "+logsize+" (logid: "+logid+") Error: "+err);
-  });
+    return testEditLog(label, token, log);
+  }).then(log => testGenerateSignedURL(label, token, log)
+    .then(log => testAddEntries(label, token, log, logsize))
+    .then(log => testEditEntries(label, token, log))
+    .then(log => testDeleteEntries(label, token, log))
+    .then(log => testDeleteLog(label, token, log)).then(() => {
+      console.log("WORKS for log size: "+logsize+" (logid: "+logid+")");
+    }).catch((err) => {
+      console.log("FAILED for log size: "+logsize+" (logid: "+logid+") Error: "+err);
+    });
 }
 setInterval(startTest, 1000);
